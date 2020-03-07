b2a997a8c2675f9a24e3dd4c59730f5696389dc7
saulmejiaalvarado <saul.meji210995@gmail.com>
2020-03-07 14:29:10 -0600

    se agrega proyecto inicial

---
+++ src/test/java/com/uatx/inventarios/InventariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uatx.inventarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
