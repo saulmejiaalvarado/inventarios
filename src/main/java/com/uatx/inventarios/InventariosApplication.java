8ec460178474828423a13e9049ce2ac572bba5ec
saulmejiaalvarado <saul.meji210995@gmail.com>
2020-03-14 13:46:04 -0600

    se agrega estrucura basica de paquetes con clases en blanco

--- src/main/java/com/uatx/inventarios/InventariosApplication.java
+++ src/main/java/com/uatx/inventarios/InventariosApplication.java
@@ -1,12 +1,21 @@
 package com.uatx.inventarios;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class InventariosApplication {
 
+	private static final Logger LOGGER= LoggerFactory.getLogger(InventariosApplication.class);
+
 	public static void main(String[] args) {
+
+		Integer x = 10;
+		Integer y = 20;
+		LOGGER.debug("Hola desde el loger {} {}",x,y);
+
 		SpringApplication.run(InventariosApplication.class, args);
 	}
 
